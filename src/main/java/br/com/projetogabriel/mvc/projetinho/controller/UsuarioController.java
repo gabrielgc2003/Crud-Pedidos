7c4fa451952be999cacef075a433ffd704298dd5
Gabriel Garcia Couto <gabrielgarciaacouto@gmail.com>
2022-08-11 11:45:30 -0300

    Alterando templates para pedidos do usuário e deixando a /home para todos os pedidos.

--- src/main/java/br/com/projetogabriel/mvc/projetinho/controller/UsuarioController.java
+++ src/main/java/br/com/projetogabriel/mvc/projetinho/controller/UsuarioController.java
@@ -1,45 +1,40 @@
 package br.com.projetogabriel.mvc.projetinho.controller;
+
 import java.security.Principal;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.projetogabriel.mvc.projetinho.model.Pedido;
 import br.com.projetogabriel.mvc.projetinho.model.StatusPedido;
 import br.com.projetogabriel.mvc.projetinho.repository.PedidoRepository;
 
-
-
 @Controller
-@RequestMapping("/home")
-public class HomeController {
-      
+@RequestMapping("usuario")
+public class UsuarioController {
       @Autowired
       private PedidoRepository pedidoRepository;
 
-      @GetMapping
+      @GetMapping("pedido")
       public String home(Model model, Principal principal){
 
-            List<Pedido> pedidos = pedidoRepository.findAll();
+            List<Pedido> pedidos = pedidoRepository.findAllByUsuario(principal.getName());
             model.addAttribute("pedidos", pedidos);
-            return "home";
+            return "usuario/home";
       }
-      @GetMapping("/{status}")
-      public String porStatus(@PathVariable("status") String status, Model model){
+      @GetMapping("pedido/{status}")
+      public String porStatus(@PathVariable("status") String status, Model model, Principal principal){
 
-            List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.valueOf(status.toUpperCase()));
+            List<Pedido> pedidos = pedidoRepository.findByStatusUsuario(StatusPedido.valueOf(status.toUpperCase()), principal.getName());
             model.addAttribute("pedidos", pedidos);
             model.addAttribute("status", status);
-            return "home";
+            return "usuario/home";
       }
       @ExceptionHandler(IllegalArgumentException.class)
       public String onError(){
-            return "redirect:/home";
+            return "redirect:/usuario/home";
       }
 }
